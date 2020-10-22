8c4e791f8c5d0d22358c85109d1eff781a4d2c07
Henry <hnr.leto11@gmail.com>
2020-10-22 16:30:06 -0400

    set up views

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,29 @@
+class UsersController < ApplicationController
+  def index
+
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  def update
+
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name)
+  end
+end
