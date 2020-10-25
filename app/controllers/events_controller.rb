6f042c5806a77b0955605798582eb58ee0593829
Henry <hnr.leto11@gmail.com>
2020-10-25 09:34:09 -0400

    event new view

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,6 @@
 class EventsController < ApplicationController
   def index
-    @event = Event.all
+    @event = Event.all.order('created_at DESC')
     @user = User.find(session[:user_id])
   end
 
@@ -8,6 +8,25 @@ class EventsController < ApplicationController
     @event = Event.find(params[:id])
   end
 
-  def create
+  def new
+    @event = Event.new
+    @user = User.find(session[:user_id])
   end
+
+  def create
+    @event = Event.new
+    if @event.save
+      flash[:notice] = 'New Event Created!'
+    else
+      flash[:alert] = 'Failed to Create Event...'
+      render 'new'
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:user).permit(:name, :description, :date)
+  end
+
 end
