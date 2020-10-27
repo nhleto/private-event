c342d0a08a8a5171d2c37d5ae3b7a00049379938
Henry <hnr.leto11@gmail.com>
2020-10-27 12:23:03 -0400

    event scope past and previous

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,16 @@
 class Event < ApplicationRecord
   belongs_to :creator, class_name: 'User'
-  has_many :invitations, foreign_key: 'attended_event_id'
+  has_many :invitations, foreign_key: 'attended_event_id', dependent: :destroy
   has_many :attendees, through: :invitations
+
+  scope :past_events, -> { where('date < ?', Date.today) }
+  scope :upcoming_events, -> { where('date > ?', Date.today) }
+
+  # def self.past_events
+  #   where('date < ?', Date.today)
+  # end
+
+  # def self.upcoming_events
+  #   where('date > ?', Date.today)
+  # end
 end
